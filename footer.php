58e71b0ecd127fa2ca7e18bbb4d04d31b4cab592
Lhachhen12 <shreejanlama608@gmail.com>
2024-08-28 14:05:38 +0545

    error: fetching data error solved

---
+++ footer.php
@@ -0,0 +1,48 @@
+<footer
+    class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-green text-light">
+    <div class="d-flex flex-column">
+        <span class="footer-text">&copy; St. Lawrence College <br /><span>
+            5th semester group</span></span>
+        <span class="footer-text mt-2">chabahil, Kathmandu, Nepal</span>
+        <span class="footer-text">Phone: 014469666</span>
+        <span class="footer-text">Email: <a href="mailto:info@stlawrencecollege.edu.np" class="text-light">info@stlawrencecollege.edu.np</a></span>
+    </div>
+    <ul class="nav justify-content-end list-unstyled d-flex">
+        <li class="ms-3">
+            <a class="text-light footer-icon" target="_blank" href="footdirect.php">
+                <i class="bi bi-telephone"></i>
+                <span class="d-none d-sm-inline ms-1"></span>
+            </a>
+        </li>
+        <li class="ms-3">
+            <a class="text-light footer-icon" target="_blank" href="https://www.facebook.com/stlawrencecollege">
+                <i class="bi bi-facebook"></i>
+                <span class="d-none d-sm-inline ms-1"></span>
+            </a>
+        </li>
+        <li class="ms-3">
+            <a class="text-light footer-icon" target="_blank" href="https://www.instagram.com/stlawrenceofficial/">
+                <i class="bi bi-instagram"></i>
+                <span class="d-none d-sm-inline ms-1"></span>
+            </a>
+        </li>
+    </ul>
+</footer>
+
+<style>
+    .footer-text {
+        font-size: 14px;
+        line-height: 1.5;
+    }
+    .footer-icon {
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+    }
+    .footer-icon i {
+        font-size: 18px;
+    }
+    .bg-green {
+        background-color: #006400; 
+    }
+</style>

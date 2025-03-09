33299f0907d3720d2253688c12a3b7b34b1f4e12
Lhachhen12 <shreejanlama608@gmail.com>
2025-03-09 14:55:58 +0545

    qr image added

--- payment.php
+++ payment.php
@@ -30,7 +30,7 @@
         </div>
         <div class="row row-cols-1 row-cols-md-2 mb-5">
             <div class="col mb-3 qr mb-md-0">
-                <img src="img/qr.png" class="img-fluid rounded-25 float-start" alt="qr">
+                <img src="img/qr1.jpg" class="img-fluid rounded-25 float-start" alt="qr">
 
             </div>
 
